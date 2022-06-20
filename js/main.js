be56083b88d43a158759a87104ee6f1b742d4aef
bogdanlotuk <bogdanlotuk@gmail.com>
2022-06-20 21:39:24 +0300

    adaptive services

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(function () {
 
+  $('.burger').click(function() {
+    $('.menu__list').toggleClass('menu__list--active')
+    $('.burger').toggleClass('burger--active')
+    $('.logo').toggleClass('logo--active')
+  })
+
   var mixer = mixitup('.portfolio__gallery');
 });
