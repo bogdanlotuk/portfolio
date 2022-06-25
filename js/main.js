5c0069d380c99cb2334b4f95b9d1b260b4343869
bogdanlotuk <bogdanlotuk@gmail.com>
2022-06-25 17:20:38 +0300

    adaptive 7

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(function () {
+  
+  $('.burger').click(function() {
+    $('.burger').toggleClass('burger--active');
+    $('.menu__list').toggleClass('menu__list--active');
+    $('.logo').toggleClass('logo--active');
+    $('.title').toggleClass('title--active');
+  })
+
+  $('.menu__link').click(function() {
+    $('.burger--active').removeClass('burger--active');
+    $('.menu__list--active').removeClass('menu__list--active');
+    $('.logo--active').removeClass('logo--active');
+    $('.title--active').removeClass('title--active');
+  })
 
   var mixer = mixitup('.portfolio__gallery');
 });
